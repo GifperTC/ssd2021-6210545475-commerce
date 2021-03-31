7f893f7d29defdaacc678c948b5554d717328b4a
GifperTC <59831047+GifperTC@users.noreply.github.com>
2021-03-31 14:28:57 +0700

    fix bugs

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,8 +1,7 @@
 class WelcomesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+
   def index
     @search = params[:search]
-
     @products = Product.all
     @products = @products.where("title like ? or description like ?", "%#{@search}%", "%#{@search}%") if @search.present?
     @products = @products.page(params[:page]).per(5)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  root 'welcomes#index'
-  resources :welcomes
   devise_for :users
+  root 'welcomes#index'
+
   resources :products
   resources :categories
+
 end
