28f76a87ed5d7c492862d21485cbe19ee2332eba
GifperTC <59831047+GifperTC@users.noreply.github.com>
2021-04-09 21:58:10 +0700

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,6 @@ class Product < ApplicationRecord
   has_many :categories, through: :product_categories
 
   validates :description, length: { minimum: 4 }
-  validates :stock, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock, :numericality => { :greater_than_or_equal_to => 0 }
 
 end
